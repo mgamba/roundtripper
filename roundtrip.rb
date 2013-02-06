595fdd78ad60192e4e6e398d4ebf8bad85ebc86c
Michael Gamba <mike@mysqlwtf.com>
2013-02-06 01:32:24 -0500

    rewrite to find just 2-legged round trips

--- roundtrip.rb
+++ roundtrip.rb
@@ -1,6 +1,5 @@
-#! /usr/bin/ruby
+#! /usr/bin/env ruby
 
-DMAX = 2
 #FNAME = "JetBlue   The Holiday Recovery Sale.html"
 #REGX = /Now\([^,],(\d*),&quot;(\w*)&quot;,&quot;(\w*)/
 
@@ -10,29 +9,33 @@ DMAX = 2
 FNAME = "JetBlue   The Fly Somewhere New Sale.html"
 REGX = /data-od\="([^,]*),([^"]*)[^\$]*\$([\d]*)/
 
-def show flights, depth=0, ttrip=nil
-  round_trips = []
-  if depth == 0
-    n = flights.map{|f|f if f[1]==ARGV[0]}.compact.count
-    puts "#{n} flights leaving #{ARGV[0]}"
-    i = 0
-  end
-  flights.each do |flight|
-    if (depth<DMAX && depth>0) || (depth==0 && ARGV[0]==flight[1])
-      if depth == 0
-        print "\b\b\b\b#{(100.0*i/n).round}%"
-        i += 1
-      end
-      ttrip = [flight[1]] unless depth > 0
-      trip = ttrip.dup
-      trip << flight [0] << flight[2] if (flight[1]==trip[-1] && (!trip.include?(flight[2]) || flight[2]==trip[0]))
-      if trip[0] == trip[-1]
-        round_trips << trip.join("-")
-        break if depth > 0
-      else
-        round_trips << show(flights.map{|f|f if f[2]!=flight[2] && trip[-3]!=f[1] }.compact, depth+1, trip)
-      end
-    end
+def flights_from(airport)
+  @flights.select{|f|f[0]==airport}
+end
+
+def flights_to(airport)
+  @flights.select{|f|f[1]==airport}
+end
+
+def connections(from, to)
+  @flights.select{|f|f[0]==from[1] && f[1]==to[0]}
+end
+
+def connections_to_any_of(from, to)
+  to.select{|f|f[0]==from[1]}
+end
+
+def trips_with_2_legs
+  departing_flights = flights_from(@origin)
+  returning_flights = flights_to(@origin)
+  flights = departing_flights.map{|f|connections_to_any_of(f,returning_flights)}
+  trips = departing_flights.map.zip(flights.map(&:first)).reject{|e|e.last.nil?}
+  flight_count = trips.count
+
+  print "\b"*20
+  puts "\n*** #{flight_count} roundtrip flights with 2 legs ***"
+  trips.each do |trip|
+    puts "#{@origin} - #{trip.last.first} - #{@origin}   $#{trip.last.last.to_i+trip.first.last.to_i}"
   end
   round_trips
 end
@@ -41,9 +44,8 @@ def main
   if ARGV.empty?
     puts "need to specify final destination.  ex: ruby rountrip.rb JFK"
   else
-    flights = show(File.open(FNAME, "rb").read.scan(REGX).compact).flatten.compact.uniq.sort
-    print "\b"*20
-    puts "#{flights.count > 0 ? '*** ' : nil}#{flights.count} roundtrip flights with maximum of #{DMAX} legs"
-    puts flights
+    @flights = File.open(FNAME, "rb").read.scan(REGX).compact
+    @origin = ARGV[0]
+    flights = trips_with_2_legs
   end
 end;main
